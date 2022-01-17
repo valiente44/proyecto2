25ec1ce395ff39a1bb9a59b3b3a5b86e49ffe459
Daniel Valiente <91016774+valiente44@users.noreply.github.com>
2022-01-17 16:40:37 +0100

    Add files via upload

---
+++ vistas/script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    $( document ).ready(function() {
+        $('#modalForm').modal('hide'); hola edu :v uwu uwu uwu uwu uwu uwu uwu
+        });
+        
+    
+}
+
+
+
